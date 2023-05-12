027da570e7ca4db81dd6cccb91e8b71aefbe50d3
danilopgon <daniel.lopezgonzalez.94@gmail.com>
2023-05-12 11:27:07 +0200

    Update readme and replace live-server with serve

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const TASK_CLASS =
   "rounded-lg border border-b-4 border-r-4 border-black bg-gray-200 p-6 text-center text-gray-900 shadow-lg duration-300 ease-out hover:scale-105 hover:bg-lime-200 hover:text-gray-900 hover:shadow-sm active:bg-lime-600";
 
 const title = document.querySelector("h1");
+const addToDo = document.getElementById("addToDo");
 const taskList = document.querySelector("ul");
 
 const titleCheck = () => {
@@ -26,8 +27,6 @@ const setTaskToDo = (inputText) => {
   listItem.className = TASK_CLASS;
   taskList.appendChild(listItem);
 };
-
-const addToDo = document.getElementById("addToDo");
 
 addToDo.addEventListener("keydown", (event) => {
   if (event.key === "Enter") {
